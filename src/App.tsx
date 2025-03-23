fc5e3600ae583f75258dda06bed6aa0f4063b6af
Mithun <mithun.wys@gmail.com>
2025-03-23 12:18:27 +0530

    fix: Vercel deploy issues -  remove the unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from 'react-hot-toast';
-import { useState, useEffect } from 'react';
 import './App.css';
 import './styles/app.css';
 import { AppProvider, useAppContext } from './context/AppContext';
